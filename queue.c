9520b59614bec38e5e6ddb164e5083b8d687a9b5
ppodds <oscar20020629@gmail.com>
2022-02-16 21:21:47 +0800

    Feat: implement q _delete_mid function

--- queue.c
+++ queue.c
@@ -152,8 +152,24 @@ int q_size(struct list_head *head)
  */
 bool q_delete_mid(struct list_head *head)
 {
-    // https://leetcode.com/problems/delete-the-middle-node-of-a-linked-list/
+    if (!head || list_empty(head))
+        return NULL;
+
+    struct list_head *slow;
+    struct list_head *fast = head->next;
+
+    list_for_each (slow, head) {
+        // find the middle node
+        if (fast == head || fast->next == head)
+            break;
+        fast = fast->next->next;
+    }
+    list_del(slow);
+    element_t *node = container_of(slow, element_t, list);
+    free(node->value);
+    free(node);
     return true;
+    // https://leetcode.com/problems/delete-the-middle-node-of-a-linked-list/
 }
 
 /*
