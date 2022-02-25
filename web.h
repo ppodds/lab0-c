9a8d78b716cad7224a1e1c2915f03a985ce7dc86
ppodds <oscar20020629@gmail.com>
2022-02-25 21:08:17 +0800

    Fix: web.h macro define name

--- web.h
+++ web.h
@@ -1,5 +1,5 @@
-#ifndef LAB0_TINY_H
-#define LAB0_TINY_H
+#ifndef LAB0_WEB_H
+#define LAB0_WEB_H
 #include <netinet/in.h>
 
 /* Simplifies calls to bind(), connect(), and accept() */
@@ -12,4 +12,4 @@ int open_listenfd(int port);
 
 char *process(int connfd, struct sockaddr_in *clientaddr);
 
-#endif /* LAB0_TINY_H */
+#endif /* LAB0_WEB_H */
